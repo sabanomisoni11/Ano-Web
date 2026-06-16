463235842b919014a71f5c9156b44e2b8a53b56c
noname-11-git <sousi44sousi44@gmail.com>
2026-06-16 14:02:57 +0900

    Initial settings

---
+++ ano-web/src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-slate-100">
+      <h1 className="text-4xl font-bold text-blue-600 drop-shadow">
+        Ano-Webの開発環境へようこそ！
+      </h1>
+      <p className="mt-4 text-gray-600 text-lg">
+        この文字が大きく、青色になっていればTailwind CSSの導入は成功です。
+      </p>
+    </div>
+  )
+}
+
+export default App
